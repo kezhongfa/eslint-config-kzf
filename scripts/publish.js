8b59141076d116f93ef82a5a8058332614f95182
柯忠发 <zhongfa.ke@shuyun.com>
2020-07-18 17:10:23 +0800

    feat: 构建脚本test

--- scripts/publish.js
+++ scripts/publish.js
@@ -8,12 +8,12 @@ shell.cp('package.json', 'dist');
 
 shell.cp('README.md', 'dist');
 
-shell.exec("npm config get registry", function (code, stdout) {
-  if (!stdout.includes("registry.npmjs.org")) {
-    shell.exec("npm config set registry=https://registry.npmjs.org");
+shell.exec('npm config get registry', (_, stdout) => {
+  if (!stdout.includes('registry.npmjs.org')) {
+    shell.exec('npm config set registry=https://registry.npmjs.org');
   }
 });
 
-shell.exec("npm publish --access=public dist");
+shell.exec('npm publish --access=public dist');
 
-shell.exec("curl -X PUT https://npm.taobao.org/sync/eslint-config-kzf");
+shell.exec('curl -X PUT https://npm.taobao.org/sync/eslint-config-kzf');
