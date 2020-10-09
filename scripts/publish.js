23ab4564ac2b291f260767d963f8610cbcd00e55
kezhongfa <zhongfa.ke@shuyun.com>
2020-10-09 13:27:45 +0800

    chore: 发布脚本优化

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,23 +1,85 @@
-const shell = require('shelljs');
-const colors = require('colors');
-const argv = require('./argv');
+const shell = require("shelljs");
+const colors = require("colors");
+const fs = require("fs");
+const path = require("path");
+const argv = require("./argv");
 
-const { version, tag = 'latest' } = argv;
+const { version, tag = "latest" } = argv;
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+const execAsyncEndTasks = [];
 
 if (version) {
-  shell.exec(`npm version ${version} -m "chore: version %s"`);
-  shell.rm('-rf', 'dist');
-  shell.cp('-r', 'src', 'dist');
-  shell.cp('package.json', 'dist');
-  shell.cp('README.md', 'dist');
-  shell.exec('npm config get registry', (_, stdout) => {
-    if (!stdout.includes('registry.npmjs.org')) {
-      shell.exec('npm config set registry=https://registry.npmjs.org');
-    }
-    shell.exec(`npm publish --access=public dist --tag ${tag}`);
-    shell.exec('curl -X PUT https://npm.taobao.org/sync/eslint-config-kzf');
-    shell.exec("git push --follow-tags");
-  });
+  const execVersionTask = () =>
+    execAsync(`npm version ${version} -m "chore: version %s"`, "execVersionTask");
+  const execPackageJsonTask = () => {
+    const packageJson = require("../package.json");
+    const pkg = JSON.parse(JSON.stringify(packageJson));
+    Reflect.deleteProperty(pkg, "devDependencies");
+    Reflect.deleteProperty(pkg, "scripts");
+    const distPath = resolveApp("dist");
+    fs.writeFileSync(`${distPath}/package.json`, JSON.stringify(pkg, null, 2), {
+      encoding: "utf-8",
+    });
+  };
+  const execCopyTask = () => {
+    shell.cp('-r', 'src', 'dist');
+    shell.cp("README.md", "dist");
+  };
+  const execNpmConfigTask = () => execAsync("npm config get registry", "execNpmConfigTask");
+  const execPublishTask = () =>
+    execAsync(`npm publish --access=public dist --tag ${tag}`, "npmRegistry");
+  const execSyncTaoBaoTask = () =>
+    execAsync(
+      "curl -X PUT https://npm.taobao.org/sync/kzf-business-components",
+      "execSyncTaoBaoTask"
+    );
+  const execPushTagTask = () => execAsync("git push --follow-tags", "execPushTagTask");
+  execVersionTask()
+    .then(execCopyTask)
+    .then(execPackageJsonTask)
+    .then(execNpmConfigTask)
+    .then(execPublishTask)
+    .then(execSyncTaoBaoTask)
+    .then(execPushTagTask)
+    .then(() => {
+      console.log("发布成功".green);
+    })
+    .finally(() => {
+      if (execAsyncEndTasks.length > 0) {
+        execAsyncEndTasks.forEach((task) => {
+          task();
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(colors.red(err));
+      process.exit(1);
+    });
 } else {
-  console.error(colors.red('你未指定version 参数'));
+  console.error(colors.red("未指定version参数"));
+}
+
+function execAsync(command, execLog) {
+  return new Promise((resolve, reject) => {
+    const child = shell.exec(command, { async: true });
+    console.log(`${execLog} start`.green);
+    child.stdout.on("data", function (data) {
+      console.log(`${execLog} stdout ${data}`.yellow);
+      if (execLog === "execNpmConfigTask" && !data.includes("registry.npmjs.org/")) {
+        shell.exec("npm config set registry=https://registry.npmjs.org/");
+        execAsyncEndTasks.push(() => shell.exec(`npm config set registry=${data}`));
+      }
+    });
+    child.on("close", function (code) {
+      if (code === 0) {
+        console.log(`${execLog} end`.green);
+        resolve();
+      } else {
+        const error = `${execLog} fail`.red;
+        reject(error);
+      }
+    });
+  });
 }
